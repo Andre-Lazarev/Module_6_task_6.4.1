38ea70569f7e171cd80d18638c1afbbbdbcbcb02
Tarlord <lazarev.morton@gmail.com>
2022-01-16 15:29:08 +0300

    first commit on new branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String args[]) {
         SubClass s = new SubClass(10, 12);
-        System.out.println(s.add(10));
+        //System.out.println(s.add(10));
+        s.test();
     }
 }
