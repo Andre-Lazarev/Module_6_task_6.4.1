1f4bf4679e994e153516e0a61a4df112de463f44
Tarlord <lazarev.morton@gmail.com>
2022-01-16 15:06:51 +0300

    first commit

---
+++ src/SuperClass.java
@@ -0,0 +1,16 @@
+public class SuperClass {
+    public int a;
+    public int b;
+
+    SuperClass(int a) {
+        this.a = 15;
+        this.b = 33;
+    }
+
+    public int add(int v) {
+        this.a += v;
+        return a;
+    }
+}
+
+
